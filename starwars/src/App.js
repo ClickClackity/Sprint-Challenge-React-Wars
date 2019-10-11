d9f0c40e20323bb4e9fa241fa48bdd9a5b44cefc
Clackity <lockout18@yahoo.com>
2019-10-11 13:51:20 -0500

    mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import DisplayCard from './components/DisplayCard';
+import { Row, CardGroup} from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+ 
+    const [starWarsPerson, setStarWarsPerson] = useState();
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+    
+    
+    useEffect(() => {
+      axios
+        .get("https:///swapi.co/api/people/")
+        .then(response => {
+          setStarWarsPerson(response.data.results);
+        })
+        .catch(error => `Error: ${error}`);
+    }, []);
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-    </div>
-  );
-}
+    if(!starWarsPerson){
+       return (
+          <div className="App">
+            <h1 className="Header">React Wars</h1>
+            
+            <div className="loader">
+              <h2>Loading.. </h2>
+           </div>
+        </div>
+    );
+
+    }
+  
+    return (
+      <div className="App">
+        <h1 className="Header">React Wars</h1>
+        <Row>
+          <CardGroup>
+            {starWarsPerson.map((person, index) => {
+              return <DisplayCard key={index} person={person} />;
+            })}
+          </CardGroup>
+        </Row>
+      </div>
+    );
+};
 
 export default App;
